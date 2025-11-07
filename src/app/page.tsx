98bcff6a3f6a65576c4ab4a0402b539d44032111
Cesar Henrique Batista Camillo <cesarcamillo@MacBook-Air-de-Cesar.local>
2025-11-07 01:08:33 -0300

    add react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,39 @@
 "use client"; 
 
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 type HealthStatus = {
   status: string;
   message: string;
 } | null;
 
+const getHealth = async () => {
+  const res = await fetch("http://localhost:3001/health");
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+  return res.json();
+};
+
+
+
 export default function Home() {
-  const [apiStatus, setApiStatus] = useState<HealthStatus>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Function to fetch data
-    const getHealth = async () => {
-      try {
-        const res = await fetch("http://localhost:3001/health");
-        
-        if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        
-        const data = await res.json();
-        setApiStatus(data);
-      } catch (e: any) {
-        // This is where you'll catch the CORS error
-        console.error("Fetch error:", e);
-        setError(e.message);
-      }
-    };
-
-    getHealth();
-  }, []); // Empty dependency array means this runs once on mount
+  const query = useQuery({ queryKey: ['status'], queryFn: getHealth })
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-8">Read-it Client</h1>
       <div className="p-6 bg-gray-800 rounded-lg">
         <h2 className="text-2xl mb-4">API Status:</h2>
-        {error && (
+        {query.error && (
           <p className="text-red-400">
-            <strong>Error:</strong> {error}
+            <strong>Error:</strong> {query.error.message}
           </p>
         )}
-        {apiStatus ? (
-          <pre className="text-green-300">{JSON.stringify(apiStatus, null, 2)}</pre>
+        {query.data ? (
+          <pre className="text-green-300">{JSON.stringify(query.data, null, 2)}</pre>
         ) : (
-          !error && <p className="text-yellow-300">Fetching status...</p>
+          !query.error && <p className="text-yellow-300">Fetching status...</p>
         )}
       </div>
     </main>
